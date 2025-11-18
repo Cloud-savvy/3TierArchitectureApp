954a5aee3d80db37d315ff4aa1fd6b779afa5945
Karthick Chaithanya <146309313+Cloud-savvy@users.noreply.github.com>
2025-11-19 00:25:19 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'db-3-tier.cqfi2a08qf0m.us-east-1.rds.amazonaws.com',
     DB_USER : 'dbadmin',
     DB_PWD : 'password123',
-    DB_DATABASE : 'DB-3-tier'
+    DB_DATABASE : 'webappdb'
 });
